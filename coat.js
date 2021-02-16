a1c6f8338c6e760ef5757406195c91ad57f29710
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-16 14:03:08 +0100

    coat.js final

--- coat.js
+++ coat.js
@@ -1,18 +1,18 @@
 let isRainy = true
 let isWindy = false
 let isCloudy = false
-let temperature = 0
+let temperature = 19
 
-if (isRainy || isWindy) {
-  console.log('Mettez votre manteau')
-} else if (temperature <= 18) {
-  console.log('Mettez votre manteau, il va faire froid')
-} else if (Cloudy) {
-  console.log('Mettez peut etre votre manteau...')
-} else if (isRainy || isWindy || isCloudy || temperature <= 0) {
+if (isRainy && isWindy && isCloudy && temperature <= 0) {
   console.log('Attention au verglas')
+} else if (temperature <= 18) {
+  console.log('Mettez votre manteau, il va faire frais')
+} else if (isCloudy) {
+  console.log('Ciel couvert attenton au precipitations')
+} else if (isRainy) {
+  console.log('Mettez votre imperméable')
+} else if (isRainy || isWindy) {
+  console.log('Mettez votre manteau')
 } else {
   console.log('Nous n avez pas besoin de manteau today')
 }
-
-// exercice non finis
