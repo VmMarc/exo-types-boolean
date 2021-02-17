ca36993817d1d4ef70aa8bbad935cf12aedef5cb
bilaz666 <victor.me.marc@gmail.com.com>
2021-02-17 18:05:03 +0100

    erreur

--- showChar.js
+++
@@ -1,7 +0,0 @@
-let msg = (`C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`)
-console.log(msg)
-console.log(`Gustave Flaubert - Salammbô \n\n La longueur de cet extrait est: ${msg.length}`)
-
-for (let i = 0; i < msg.length; i++)
-  console.log(`character ${msg[i]} is at index: ${i}`)
-
